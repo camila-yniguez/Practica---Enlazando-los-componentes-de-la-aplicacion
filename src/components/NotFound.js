686d96b01e8c3b8fe85fc90e399e2dee542c81a9
camila-yniguez <cami.yniguez@gmail.com>
2023-06-22 11:30:52 -0300

    add binding the application components

---
+++ src/components/NotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const NotFound = () => {
+    return (
+        <div className=" text-danger" >
+            <h1 className="text-center">Page not found</h1>
+        </div>
+    )
+}
+export default NotFound;
